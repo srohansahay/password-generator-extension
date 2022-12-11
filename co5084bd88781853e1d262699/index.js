52a2b9fbf8bf700da403d46a156f6b9d4c590a1d
wulfric012 <rohansahay281@gmail.com>
2022-12-12 01:16:52 +0530

    first commit

---
+++ co5084bd88781853e1d262699/index.js
@@ -0,0 +1,27 @@
+function generate() {
+    const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+    let password = ""
+    for(let i=0;i<15;i++)
+    {
+        let index = Math.floor(Math.random()*91)
+        password = password + characters[index]
+    }  
+    return password
+}
+
+function buttonClick() {
+  let password11 = document.getElementById("password1")
+let password12 = document.getElementById("password2")
+    password11.textContent = generate()
+    password12.textContent = generate()
+}
+
+
+
+
+
+
+
+
+
